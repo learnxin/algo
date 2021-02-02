b4a584436ae762653c4e6576b13558123acad171
wangran.asiainfo <591367010@qq.com>
2021-02-03 01:22:12 +0800

    mergesort&quicksort

--- src/main/java/algo/sort/TestSort.java
+++ src/main/java/algo/sort/TestSort.java
@@ -118,4 +118,121 @@ public class TestSort {
             a[p] = num;
         }
     }
+
+    @Test
+    public void mergeSortTest(){
+        int[] sort={8,3,2,5,4,3,2,1};
+        mergeSort(sort,0,sort.length-1);
+        System.out.println(Arrays.toString(sort));
+    }
+
+    /**
+     * 对数组下标p到r的部分进行归并排序 merge sort
+     * @param a
+     * @param p
+     * @param r
+     */
+    public void mergeSort(int[] a,int p,int r){
+        if(p>=r){
+            return;
+        }
+        int q = (p+r)/2;
+        mergeSort(a,p,q);
+        mergeSort(a,q+1,r);
+        merge(a,p,q,r);
+    }
+
+    private void merge(int[] a, int p, int q, int r) {
+        System.out.println(Arrays.toString(a));
+        int[] c = new int[r-p+1];
+        // c的指针
+        int cp = 0;
+        int i = p;
+        int j = q+1;
+        while (i<=q && j<= r){
+            if(a[i]<a[j]){
+                c[cp++]=a[i++];
+            }else {
+                c[cp++]=a[j++];
+            }
+        }
+        if(j>r){
+            for (;i<=q;i++){
+                c[cp++]=a[i];
+            }
+        }else {
+            for (;j<=r;j++){
+                c[cp++]=a[j];
+            }
+        }
+        //复制数组
+        for (int k = 0; k < c.length; k++) {
+            a[p+k]=c[k];
+        }
+    }
+
+    private static void merge2(int[] arr, int left, int q, int right) {
+        int[] leftArr = new int[q - left + 2];
+        int[] rightArr = new int[right - q + 1];
+
+        for (int i = 0; i <= q - left; i++) {
+            leftArr[i] = arr[left + i];
+        }
+        // 第一个数组添加哨兵（最大值）
+        leftArr[q - left + 1] = Integer.MAX_VALUE;
+
+        for (int i = 0; i < right - q; i++) {
+            rightArr[i] = arr[q + 1 + i];
+        }
+        // 第二个数组添加哨兵（最大值）
+        rightArr[right - q] = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+        while (k <= right) {
+            // 当左边数组到达哨兵值时，i不再增加，直到右边数组读取完剩余值，同理右边数组也一样
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+    }
+
+    /**
+     * 快速排序
+     */
+    @Test
+    public void quickSortTest(){
+        int[] sort={8,3,2,5,4,3,2,1,4};
+        quickSort(sort,0,sort.length-1);
+        System.out.println(Arrays.toString(sort));
+    }
+
+    public void quickSort(int[] a,int p,int r){
+        if(p>=r){
+            return;
+        }
+        int q= partition(a,p,r);
+        quickSort(a,q+1,r);
+        quickSort(a,p,q-1);
+
+    }
+
+    private int partition(int[] a, int p, int r) {
+        int pivot = a[r];
+        int i=p,j=p;
+        for (;j<r;j++){
+            if(a[j]<pivot){
+                int b = a[j];
+                a[j] = a[i];
+                a[i++] = b;
+            }
+        }
+
+        a[r] = a[i];
+        a[i] = pivot;
+        return i;
+    }
 }
