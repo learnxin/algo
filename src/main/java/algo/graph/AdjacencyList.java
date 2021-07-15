0a2fdef1bcbbd3522b9480f395c9cd1489e1cfd4
learnxin <xixinwr@163.com>
2021-07-15 20:37:01 +0800

    add graph note; 求n度友好关系的demo补充

--- src/main/java/algo/graph/AdjacencyList.java
+++ src/main/java/algo/graph/AdjacencyList.java
@@ -42,6 +42,16 @@ public class AdjacencyList {
      * queue,遍历完一个vertex的edge后存放下次遍历的vertex
      * visited 顶点是否遍历过
      * prev  顶点的上顶点
+     *
+     * 广度优先搜索，通俗的理解就是，地毯式层层推进，从起始顶点开始，依次往外遍历。
+     * 广度优先搜索需要借助队列来实现，遍历得到的路径就是，起始顶点到终止顶点的最短路径。
+     * 广度优先搜索的时间复杂度都是 O(E)，空间复杂度是 O(V)
+     *
+     * 对于求取n度友好关系的问题(使用数组记录v与s的距离/层数):
+     * 1.添加另一queue，add下一层至queue当前层遍历完再从下一层取值；类似二叉树层次遍历
+     * 2.记录每一层入队和出队情况:一个变量记录每种边长的入队数量(每层的个数)，一个变量记录已出队的个数，一个变量记录当前的度数
+     * 此处为2的实现
+     * 3.当前节点与s的距离是prev距离+1，最为简洁
      * @param s
      * @param t
      */
@@ -52,9 +62,21 @@ public class AdjacencyList {
         visited[s] = true;
         int[] prev = new int[v];
         Arrays.fill(prev, -1);
+        //记录已出队数量(对头位置) 初始化为0层队头位置
+        int front = 0;
+        //记录'当前层'队中数量，此处为0层
+        int behind = queue.size();
+        //记录度数
+        int degree = 0;
+        //记录每个vertex的层数(度数)
+        int[] vdegree = new int[v];
 
         while (queue.size()>0){
             Integer poll = queue.poll();
+            //记录层数
+            vdegree[poll] = degree;
+            //队头后移(出队数+1)
+            front++;
             LinkedList<Integer> edges = adj[poll];
             for (Integer integer:edges
                  ) {
@@ -68,11 +90,23 @@ public class AdjacencyList {
                     }
                 }
             }
+            //相等即为当前层已遍历完，此时对中为下一层数据；
+            if(front == behind){
+                front = 0;
+                behind = queue.size();
+                degree++;
+            }
         }
     }
 
     /**
      * 深度优先搜索
+     * 深度优先搜索用的是回溯思想，非常适合用递归实现。
+     * 换种说法，深度优先搜索是借助栈来实现的。
+     * 在执行效率方面，深度优先搜索的时间复杂度都是 O(E)，空间复杂度是 O(V)。
+     *
+     * 对于求取n度友好关系的问题(使用数组记录v与s的距离):
+     * 递归式向下传当前度数，记录即可
      * @param s
      * @param t
      */
