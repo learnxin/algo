11428ee2ededb57fde12a17145293d93347a1956
learnxin <xixinwr@163.com>
2021-09-17 00:43:58 +0800

    Apache Common 的开源实现

---
+++ src/main/java/algo/trie/Trie.java
@@ -0,0 +1,16 @@
+package algo.trie;
+
+import org.apache.commons.collections4.trie.PatriciaTrie;
+
+/**
+ * @Author: wangran
+ * @Description: trie
+ * @Date: 2021/7/28 下午5:13
+ */
+public class Trie {
+
+    public void testTrie(){
+        //Apache Common 的开源实现
+        PatriciaTrie patriciaTrie = new PatriciaTrie();
+    }
+}
