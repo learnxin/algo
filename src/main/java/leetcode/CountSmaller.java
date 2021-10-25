c05f6ec94fb47e13cc26cae10026db0b9e143a7c
WangRan <18510328957@gts.work>
2021-10-26 02:06:25 +0800

    分治 divide and conquer

---
+++ src/main/java/leetcode/CountSmaller.java
@@ -0,0 +1,107 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 给你`一个整数数组 nums ，按要求返回一个新数组 counts 。数组 counts 有该性质： counts[i] 的值是  nums[i] 右侧小于 nums[i] 的元素的数量。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [5,2,6,1]
+ * 输出：[2,1,1,0]
+ * 解释：
+ * 5 的右侧有 2 个更小的元素 (2 和 1)
+ * 2 的右侧仅有 1 个更小的元素 (1)
+ * 6 的右侧有 1 个更小的元素 (1)
+ * 1 的右侧有 0 个更小的元素
+ * 示例 2：
+ *
+ * 输入：nums = [-1]
+ * 输出：[0]
+ * 示例 3：
+ *
+ * 输入：nums = [-1,-1]
+ * 输出：[0,0]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CountSmaller {
+    public int[] index;
+    public int[] result;
+    @Test
+    public void reversePairs(){
+        int[] nums = {5,2,6,1};
+        List<Integer> integers = countSmaller(nums);
+        System.out.println(integers);
+    }
+
+
+    public List<Integer> countSmaller(int[] nums) {
+        index = new int[nums.length];
+        /**
+         * [0,1,2,3]
+         * [3,1,0,2]
+         */
+        result = new int[nums.length];
+        for (int i = 0; i < index.length; i++) {
+            index[i] = i;
+        }
+        mergeSort(nums,0,nums.length-1);
+        List<Integer> list = new ArrayList<Integer>();
+        for (int num : result) {
+            list.add(num);
+        }
+        return list;
+    }
+
+    private void mergeSort(int[] nums, int p, int r) {
+        if(p>=r){
+          return;
+        }
+        //中间点
+        int q = p+(r-p)/2;
+        mergeSort(nums,p,q);
+        mergeSort(nums,q+1,r);
+        merge(nums,p,q,r);
+
+    }
+    // [4, 5, 6, 8][1, 2, 2, 3, 3,]
+    private void merge(int[] nums, int p, int q, int r) {
+        int[] temp = new int[r-p+1];
+        int[] tempIndex = new int[r-p+1];
+        int i = p,j=q+1,k=0;
+        while (i<=q&&j<=r){
+            if(nums[i]<=nums[j]){
+                tempIndex[k] = index[i];
+                temp[k++] = nums[i++];
+            }else {
+//                result[index[i]] += q-i+1;
+                for (int l = i; l <=q ; l++) {
+                    result[index[l]]++;
+                }
+                tempIndex[k] = index[j];
+                temp[k++] = nums[j++];
+            }
+        }
+        while (i<=q){
+            result[index[i]] += r-j+1;
+            tempIndex[k] = index[i];
+            temp[k++]= nums[i++];
+        }
+        while (j<=r){
+            tempIndex[k] = index[j];
+            temp[k++]= nums[j++];
+        }
+        for (int l = 0; l < k; l++) {
+            index[p+l] = tempIndex[l];
+            nums[p+l] = temp[l];
+        }
+    }
+}
---
+++ src/main/java/leetcode/InversionPair.java
@@ -0,0 +1,70 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+
+ * 示例 1:
+ *
+ * 输入: [7,5,6,4]
+ * 输出: 5
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 1.归并排序修改merge统计逆序数
+ * 2.桶排序统计逆序数，范围大需要离散和二分
+ */
+public class InversionPair {
+    public int num = 0;
+    @Test
+    public void reversePairs(){
+        int[] nums = {7,5,6,4};
+        int i = reversePairs(nums);
+        System.out.println(i);
+    }
+
+
+    public int reversePairs(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+        return num;
+    }
+
+    private void mergeSort(int[] nums, int p, int r) {
+        if(p>=r){
+          return;
+        }
+        //中间点
+        int q = p+(r-p)/2;
+        mergeSort(nums,p,q);
+        mergeSort(nums,q+1,r);
+        merge(nums,p,q,r);
+
+    }
+    // [4, 5, 6, 8][1, 2, 2, 3, 3,]
+    private void merge(int[] nums, int p, int q, int r) {
+        int[] temp = new int[r-p+1];
+        int i = p,j=q+1,k=0;
+        while (i<=q&&j<=r){
+            if(nums[i]<=nums[j]){
+                temp[k++] = nums[i++];
+            }else {
+                num += q-i+1;
+                temp[k++] = nums[j++];
+            }
+        }
+        while (i<=q){
+            num += r-j+1;
+            temp[k++]= nums[i++];
+        }
+        while (j<=r){
+            temp[k++]= nums[j++];
+        }
+        for (int l = 0; l < k; l++) {
+            nums[p+l] = temp[l];
+        }
+    }
+}
