8be91ad7aa0addbed4ffe7d0a739b790f5269bc4
WangRan <18510328957@gts.work>
2021-12-15 15:40:35 +0800

    EditDistance.java

---
+++ src/main/java/algo/dynamicprogramming/EditDistance.java
@@ -0,0 +1,105 @@
+package algo.dynamicprogramming;
+
+import org.junit.Test;
+
+/**
+ * @Author: wangran
+ * @Description: 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * 你可以对一个单词进行如下三种操作：
+ * char[] a,b
+ * 插入一个字符 插入a ->a[i]b[i+1] 插入b ->a[i+1]b[i]
+ * 删除一个字符 删除a ->a[i+1]b[i] 删除b ->a[i]b[i+1]
+ * 替换一个字符 替换a ->a[i+1]b[i+1] 替换b ->a[i+1]b[i+1]
+ *输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ *
+ *
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/edit-distance 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Date: 2021/12/15 上午6:56
+ */
+public class EditDistance {
+    @Test
+    public void test(){
+        int i = minDistance("horse", "ros");
+        System.out.println(i);
+    }
+
+
+    public int minDistance(String word1, String word2) {
+        char[] a = word1.toCharArray();
+        char[] b = word2.toCharArray();
+
+        //有二维数组min[i][j] [0][0]认为为空串 注意次数组填写的是[i][j]匹配完的结果而不是上一次的传值
+        int[][] min = new int[a.length+1][b.length+1];
+        //初始化数据 第0行、第0列对应直接全插入的场景
+        for (int k = 0; k <= a.length; k++) {
+            min[k][0] = k;
+        }
+        for (int k = 0; k <= b.length; k++) {
+            min[0][k] = k;
+        }
+        for (int k = 1; k < min.length; k++) {
+            for (int l = 1; l < min[k].length; l++) {
+                if(a[k-1] == b[l-1]){
+                    min[k][l] = getMin(min[k-1][l]+1,min[k][l-1]+1,min[k-1][l-1]);
+                }else {
+                    min[k][l] = getMin(min[k-1][l]+1,min[k][l-1]+1,min[k-1][l-1]+1);
+                }
+            }
+        }
+
+        return min[a.length][b.length];
+    }
+
+    private int getMin(int a,int b, int c){
+        int min = Math.min(a, b);
+        return Math.min(min, c);
+    }
+
+
+    /**
+     * 默认实现
+     * @param a
+     * @param n
+     * @param b
+     * @param m
+     * @return
+     */
+    public int lwstDP(char[] a, int n, char[] b, int m) {
+        if(n ==0 || m ==0){
+            return Math.abs(n-m);
+        }
+        int[][] minDist = new int[n][m];
+        for (int j = 0; j < m; ++j) { // 初始化第0行:a[0..0]与b[0..j]的编辑距离
+            if (a[0] == b[j]) minDist[0][j] = j;
+            else if (j != 0) minDist[0][j] = minDist[0][j-1]+1;
+            else minDist[0][j] = 1;
+        }
+        for (int i = 0; i < n; ++i) { // 初始化第0列:a[0..i]与b[0..0]的编辑距离
+            if (a[i] == b[0]) minDist[i][0] = i;
+            else if (i != 0) minDist[i][0] = minDist[i-1][0]+1;
+            else minDist[i][0] = 1;
+        }
+        for (int i = 1; i < n; ++i) { // 按行填表
+            for (int j = 1; j < m; ++j) {
+                if (a[i] == b[j]) minDist[i][j] = min(
+                        minDist[i-1][j]+1, minDist[i][j-1]+1, minDist[i-1][j-1]);
+                else minDist[i][j] = min(
+                        minDist[i-1][j]+1, minDist[i][j-1]+1, minDist[i-1][j-1]+1);
+            }
+        }
+        return minDist[n-1][m-1];
+    }
+
+    private int min(int x, int y, int z) {
+        int minv = Integer.MAX_VALUE;
+        if (x < minv) minv = x;
+        if (y < minv) minv = y;
+        if (z < minv) minv = z;
+        return minv;
+    }
+}
--- src/main/java/algo/dynamicprogramming/LengthOfLIS.java
+++ src/main/java/algo/dynamicprogramming/LengthOfLIS.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 /**
  * @Author: wangran
- * @Description: 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * @Description: 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。longestIncreaseSubArrayDP
  * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
  *示例 1：
  *
